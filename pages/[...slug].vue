9cd6575c1a8543c5528387024d6d78fb66cb94d2
danielzhang183 <danielzhang182@gmail.com>
2024-03-27 10:00:49 +0800

    feat: setup vueuse

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-
+const { x, y } = useMouse()
 </script>
 
 <template>
   <div text-green p-4>
     hello world
+    <div text-gray-400>
+      x: {{ x }}; y: {{ y }}
+    </div>
   </div>
 </template>
